65e74c26d6314dccb00605013df1bddabddbefa8
sebb94 <omgl0lwth@gmail.com>
2020-12-19 18:36:45 +0100

    picture fixes

--- src/Cars.js
+++ src/Cars.js
@@ -2,19 +2,19 @@ export const cars = [
   {
     id: 1,
     name: 'Audi',
-    file: '../img/audi.jpg',
+    file: '../react-app/img/audi.jpg',
     description: 'niemiecki producent samochodów osobowych z siedzibą w Ingolstadt w Niemczech, należący do koncernu Volkswagen AG.',
   },
   {
     id: 2,
     name: 'BMW',
-    file: '../img/bmw.jpg',
+    file: '../react-app/img/bmw.jpg',
     description: 'niemiecki koncern motoryzacyjny produkujący od 1916 roku samochody osobowe, motocykle, skutery oraz silniki.'
   },
   {
     id: 3,
     name: 'Porsche',
-    file: '../img/porsche.jpg',
+    file: '../react-app/img/porsche.jpg',
     description: 'niemieckie przedsiębiorstwo produkujące obecnie samochody sportowe oraz luksusowe samochody osobowe, założone w 1931 roku przez Ferdinanda Porsche.'
   },
 ];
